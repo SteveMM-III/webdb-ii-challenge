69a5a0e8abbf1657fd24b690256dfd3365192c0d
Kronykus <MoreheadSteve@gmail.com>
2019-12-10 16:12:56 -0600

    db

--- data/migrations/20191210154915_addCarsTable.js
+++ data/migrations/20191210154915_addCarsTable.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable( 'cars', tbl => {
+    tbl.increments();
+    tbl.integer( 'VIN',     128 ).notNullable().unique().index();
+    tbl.string ( 'make',    128 ).notNullable();
+    tbl.string ( 'model',   128 ).notNullable();
+    tbl.integer( 'mileage', 128 ).notNullable();
+
+    tbl.string( 'transmission_type' );
+  } );
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists( 'cars' );
 };
